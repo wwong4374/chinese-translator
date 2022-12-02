a59bb6c5729cbd6788bf09087dda8a0209c2fe2b
Wilson Wong <wilson@notablehealth.com>
2022-12-01 21:34:15 -1000

    delete library

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-# #wsj-article-wrap > div.article-content
+import requests
+
+def get_article_text(url: str) -> str:
+    # #wsj-article-wrap > div.article-content
+    ...
+
+url = input("Enter a url: ")
+page = requests.get(url)
+print (f"RESPONSE: {page}")
+contents = page.contents
+print(f"PAGE CONTENTS: {contents}")
